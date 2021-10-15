19cd5495e010f832fcc3c8cfc7148f727536c373
Joaquín de la Iglesia <joaquindlaiglesia@gmail.com>
2021-10-15 08:45:53 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ require_once('libs/Smarty.class.php');
 
 $smarty = new Smarty();
 
-$smarty->assign('marca',"SneakerFever");
+$smarty->assign('marca',"SneakerFeverr");
 $smarty->display('templates/header.tpl');
 $smarty->display('templates/nav.tpl');
 $smarty->display('templates/footer.tpl');
