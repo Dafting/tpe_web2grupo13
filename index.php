ec8fefdff3573f5f57a31eb95751d56bf7d1f4f9
Joaquín de la Iglesia <joaquindlaiglesia@gmail.com>
2021-10-14 22:47:02 -0300

    Revert "Update index.php"
    This reverts commit 19cd5495e010f832fcc3c8cfc7148f727536c373.

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ require_once('libs/Smarty.class.php');
 
 $smarty = new Smarty();
 
-$smarty->assign('marca',"SneakerFeverr");
+$smarty->assign('marca',"SneakerFever");
 $smarty->display('templates/header.tpl');
 $smarty->display('templates/nav.tpl');
 $smarty->display('templates/footer.tpl');
