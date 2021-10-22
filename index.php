2bcf170a9d9cf3bb2585e164c92eb056a6c01f39
Joaquín de la Iglesia <joaquindlaiglesia@gmail.com>
2021-10-22 17:53:41 -0300

    Update index.php

--- index.php
+++ index.php
@@ -2,31 +2,26 @@
     require_once('libs/Smarty.class.php');
 
     $smarty = new Smarty();
+    
+    $smarty->assign('marca',"SneakerFever");
+
+    $smarty->display('templates/header.tpl');
+    $smarty->display('templates/nav.tpl');
 
     function showIndex(Smarty $smarty) {
-        $smarty->assign('marca',"SneakerFever");
         $smarty->assign('pagina',"Inicio");
-        $smarty->display('templates/header.tpl');
-        $smarty->display('templates/nav.tpl');
         $smarty->display('templates/index.tpl');
-        $smarty->display('templates/footer.tpl');
     }
 
     function showTienda(Smarty $smarty) {
-        $smarty->assign('marca',"SneakerFever");
         $smarty->assign('pagina',"Tienda");
-        $smarty->display('templates/header.tpl');
-        $smarty->display('templates/nav.tpl');
         $smarty->display('templates/tienda.tpl');
-        $smarty->display('templates/footer.tpl');
     }
 
     function showContacto(Smarty $smarty) {
-        $smarty->assign('marca',"SneakerFever");
         $smarty->assign('pagina',"Contacto");
-        $smarty->display('templates/header.tpl');
-        $smarty->display('templates/nav.tpl');
         $smarty->display('templates/contacto.tpl');
-        $smarty->display('templates/footer.tpl');
     }
+
+    $smarty->display('templates/footer.tpl');
 ?>
