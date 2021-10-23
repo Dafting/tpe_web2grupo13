d6ff5195f50a19a3d6d4ab24dade4681dc7c675b
Joaquín de la Iglesia <joaquindlaiglesia@gmail.com>
2021-10-22 22:52:56 -0300

    Update index.php

--- index.php
+++ index.php
@@ -5,23 +5,27 @@
     
     $smarty->assign('marca',"SneakerFever");
 
-    $smarty->display('templates/header.tpl');
-    $smarty->display('templates/nav.tpl');
-
     function showIndex(Smarty $smarty) {
         $smarty->assign('pagina',"Inicio");
+        $smarty->display('templates/header.tpl');
+        $smarty->display('templates/nav.tpl');
         $smarty->display('templates/index.tpl');
+        $smarty->display('templates/footer.tpl');
     }
 
     function showTienda(Smarty $smarty) {
         $smarty->assign('pagina',"Tienda");
+        $smarty->display('templates/header.tpl');
+        $smarty->display('templates/nav.tpl');
         $smarty->display('templates/tienda.tpl');
+        $smarty->display('templates/footer.tpl');
     }
 
     function showContacto(Smarty $smarty) {
         $smarty->assign('pagina',"Contacto");
+        $smarty->display('templates/header.tpl');
+        $smarty->display('templates/nav.tpl');
         $smarty->display('templates/contacto.tpl');
+        $smarty->display('templates/footer.tpl');
     }
-
-    $smarty->display('templates/footer.tpl');
 ?>
